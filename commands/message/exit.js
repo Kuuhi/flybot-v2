06755346cecb310199e6d4c47cf962f1ce4b8d8a
Kuuhi0 <Kuuhi0@gmail.com>
2025-06-12 11:33:07 +0900

    fix exit.js

--- commands/message/exit.js
+++ commands/message/exit.js
@@ -9,7 +9,6 @@ module.exports = {
     description: 'exitを呼び出すよ',
     
     async execute(client, message, args) {
-    if (!this.adminIds.includes(message.author.id)) return
     await message.react("✅")
     const exitCode = Number(args[0]) || 0;
     process.exit(exitCode);
