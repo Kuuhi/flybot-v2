04912e7d2663d838c1ba15b50224ab3e8b08f258
Kuuhi0 <Kuuhi0@gmail.com>
2025-06-20 18:49:00 +0900

    clean messageDeleteMute.js

--- events/messageDeleteMute.js
+++ events/messageDeleteMute.js
@@ -4,9 +4,10 @@ const { Events } = require('discord.js');
 
 module.exports = {
     name: Events.MessageCreate,
+    
     execute(message) {
         if (message.member?.roles?.cache.has("898931354769707078")) {
-            message.delete().catch(error => console.error('メッセージ削除中にエラー:', error));
+            message.delete().catch(error => console.error(error));
         }
     },
 };
