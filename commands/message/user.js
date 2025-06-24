59358b07c8f00933b3cc58fa1fd58b22495cfebc
Kuuhi0 <Kuuhi0@gmail.com>
2025-06-24 22:49:14 +0900

    add user.js

---
+++ commands/message/user.js
@@ -0,0 +1,35 @@
+// commands/message/user.js
+
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'user',
+    description: '',
+    adminOnly: true,
+    aliases: ['userInfo', 'ui', 'userinfo'],
+    usage: '',
+
+    async execute(client, message, args) {
+
+        const targetUser = ( message.mentions.users.first() || await client.users.fetch(args[0]).catch(() => null) ) || message.author;
+
+        if (!targetUser) {
+            return message.reply({ content: 'ユーザーを指定してください', allowedMentions: { repliedUser: false } });
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setAuthor({ name: targetUser.tag, iconURL: targetUser.displayAvatarURL({ size: 1024, dynamic: true }), url: 'https://discord.com/users/' + targetUser.id })
+            .setDescription(`
+                ID: ${targetUser.id}
+                username: ${targetUser.username}
+                globalName: ${targetUser.globalName || '-'}
+                bot: ${targetUser.bot ? 'はい' : 'いいえ'}
+                アカウント作成: <t:${Math.floor(targetUser.createdTimestamp / 1000)}:F>
+                サーバー参加: <t:${Math.floor(message.guild.members.cache.get(targetUser.id).joinedTimestamp / 1000)}:F>
+                `)
+
+        message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+
+    },
+};
