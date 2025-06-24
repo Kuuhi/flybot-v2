b09cef8cde27c30f70203b5efc9ce14a7c8d074c
Kuuhi0 <Kuuhi0@gmail.com>
2025-06-24 22:17:40 +0900

    add icon.js

---
+++ commands/message/icon.js
@@ -0,0 +1,33 @@
+// commands/message/icon.js
+
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'icon',
+    description: '',
+    adminOnly: true,
+    usage: '',
+
+    async execute(client, message, args) {
+
+        const targetUser = message.mentions.users.first() || await client.users.fetch(args[0]).catch(() => null);
+
+        if (!targetUser) {
+            return message.reply({ content: 'kickするユーザーを指定してください', allowedMentions: { repliedUser: false } });
+        }
+
+        if (!targetUser.displayAvatarURL()) {
+            return message.reply({ content: 'そのユーザーはアイコンを設定していません   ', allowedMentions: { repliedUser: false } });
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setAuthor({ name: `${targetUser.tag} のアイコン` })
+            .setImage(targetUser.displayAvatarURL({ size: 1024, dynamic: true }))
+
+        message.reply({
+            embeds: [embed],
+            allowedMentions: { repliedUser: false }
+        });
+    },
+};
