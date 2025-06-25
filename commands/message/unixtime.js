c7f72eb8b74ade0ab1365907e0ab180deaca382e
Kuuhi0 <Kuuhi0@gmail.com>
2025-06-25 21:58:39 +0900

    update unixtime.js
    エイリアスを
    追加

--- commands/message/unixtime.js
+++ commands/message/unixtime.js
@@ -2,19 +2,15 @@
 
 module.exports = {
     name: 'unixtime',
-    aliases: ['unix','getunix'],
+    aliases: ['unix', 'getunix', 'unixTime'],
     description: '',
     usage: 'unixtime [date]',
+
     async execute(client, message, args) {
 
         const date = args[0] ? new Date(args[0]) : new Date()
         const unix = Math.floor(date.getTime() / 1000)
 
-        try {
-            message.reply({ content: String(unix), allowedMentions: { parse: [] } })
-        } catch (error) {
-            console.error(error);
-            message.reply({ content: "えらった！", allowedMentions: { parse: [] } });
-        }
+        message.reply({ content: String(unix), allowedMentions: { parse: [] } })
     },
 };
